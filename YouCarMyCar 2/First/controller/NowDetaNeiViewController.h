c54da818cc1e90d836475beb83f6b77aab1d7ca2
LLY <lly>
2015-07-24 17:32:10 +0800

    no message

---
+++ YouCarMyCar 2/First/controller/NowDetaNeiViewController.h
@@ -0,0 +1,13 @@
+//
+//  NowDetaNeiViewController.h
+//  YouCarMyCar
+//
+//  Created by LLY on 15/7/24.
+//  Copyright (c) 2015年 LLY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NowDetaNeiViewController : UIViewController
+
+@end
---
+++ YouCarMyCar 2/First/controller/NowDetaNeiViewController.m
@@ -0,0 +1,37 @@
+//
+//  NowDetaNeiViewController.m
+//  YouCarMyCar
+//
+//  Created by LLY on 15/7/24.
+//  Copyright (c) 2015年 LLY. All rights reserved.
+//
+
+#import "NowDetaNeiViewController.h"
+
+@interface NowDetaNeiViewController ()
+
+@end
+
+@implementation NowDetaNeiViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
