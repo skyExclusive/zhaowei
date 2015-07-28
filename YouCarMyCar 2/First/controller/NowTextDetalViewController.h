ff191988f50e8a26614ff27febbf39f03bc2cfc0
LLY <lly>
2015-07-28 15:27:17 +0800

    no message

---
+++ YouCarMyCar 2/First/controller/NowTextDetalViewController.h
@@ -0,0 +1,13 @@
+//
+//  NowTextDetalViewController.h
+//  YouCarMyCar
+//
+//  Created by LLY on 15/7/27.
+//  Copyright (c) 2015年 LLY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NowTextDetalViewController : UIViewController
+
+@end
