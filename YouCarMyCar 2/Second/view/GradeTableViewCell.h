735bae74515b72e69fe0779ca2b5f4c2be695ee5
wei3527722 <zw_mting@126.com>
2015-08-04 17:14:50 +0800

    no message

--- YouCarMyCar 2/Second/view/GradeTableViewCell.h
+++ YouCarMyCar 2/Second/view/GradeTableViewCell.h
@@ -19,4 +19,12 @@
 @property (weak, nonatomic) IBOutlet UIButton *xingxing4;
 @property (weak, nonatomic) IBOutlet UIButton *xingxing5;
 
+@property (nonatomic)BOOL isOne;
+@property (nonatomic)BOOL isTow;
+@property (nonatomic)BOOL isThree;
+@property (nonatomic)BOOL isFour;
+@property (nonatomic)BOOL isFive;
+
+
+
 @end
